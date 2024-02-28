59639d775802c250a4366b4636562a4025291cbd
Sergio Sánchez <sersanch@di.uc3m.es>
2024-02-28 23:13:06 +0100

    __init__.py change imports

--- src/main/python/uc3mtravel/__init__.py
+++ src/main/python/uc3mtravel/__init__.py
@@ -1,3 +1,5 @@
 """ Initialization of package module uc3m... """
 from .hotelmanager import HotelManager
 from .hotelmanagementexception import HotelManagementException
+from .hotelreservation import HotelReservation
+from .hotelstay import HotelStay
